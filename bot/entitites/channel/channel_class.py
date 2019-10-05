0b3806cbb1aa3f616595786fe62b50f183339606
CodeM01 <47262039+CodeM01@users.noreply.github.com>
2019-10-05 10:35:11 +0100

    Update channel_class.py

--- bot/entitites/channel/channel_class.py
+++ bot/entitites/channel/channel_class.py
@@ -1,1 +1,15 @@
+class Channel:
 
+    def __init__(self, user, guild_id, channel_id):
+        self.channel_id = channel_id
+        self.guild_id = guild_id
+
+        guild_data = user.guilds[self.guild_id]
+
+        for channel in guild_data["channels"]:
+            if channel["id"] == self.channel_id:
+                self.data = channel
+
+    @staticmethod
+    async def send(content):
+        pass
