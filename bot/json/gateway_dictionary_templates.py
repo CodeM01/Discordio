4b3c83f16bf0d854092720546a5756ec7ba84de4
CodeM01 <47262039+CodeM01@users.noreply.github.com>
2019-10-05 10:38:36 +0100

    Update gateway_dictionary_templates.py

--- bot/json/gateway_dictionary_templates.py
+++ bot/json/gateway_dictionary_templates.py
@@ -1,1 +1,25 @@
+basic_layout = {
+        "op": "",
+        "d": None,
+        "s": "",
+        "t": ""
+}
 
+heart_beat = {
+    "op": 1,
+    "d": None
+}
+
+identify = {
+    "token": "my_token",
+    "properties": {
+        "$os": "linux",
+        "$browser": "DiscordPyIo",
+        "$device": "DiscordPyIo"
+    }
+}
+
+new_message = {
+    "content": "Hello, World!",
+    "tts": False,
+}
