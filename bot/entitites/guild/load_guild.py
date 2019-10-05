064b81375c8a334e6e6b24bb3cd8877a950877d8
CodeM01 <47262039+CodeM01@users.noreply.github.com>
2019-10-05 10:36:18 +0100

    Update and rename guild_loading.py to load_guild.py

---
+++ bot/entitites/guild/load_guild.py
@@ -0,0 +1,11 @@
+async def load_guilds(user, input_guilds):
+
+    for guild in input_guilds:
+        if not user.guilds.get(guild["id"]):
+            # Create Guild
+            user.guilds[guild["id"]] = {}
+
+async def load_guild_data(user, guild_data):
+    await load_guilds(user, [guild_data])
+    user.guilds[guild_data["id"]] = guild_data
+    print(user.guilds[guild_data["id"]])
