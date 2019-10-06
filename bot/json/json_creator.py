b20269d655b80055d18a02afd91785573683d7e2
CodeM01 <47262039+CodeM01@users.noreply.github.com>
2019-10-06 15:52:47 +0100

    Update json_creator.py

--- bot/json/json_creator.py
+++ bot/json/json_creator.py
@@ -1,7 +1,7 @@
 import json
 from platform import system
 
-from bot.json import gateway_dictionary_templates as templates
+from RawDiscordBot.json import GatewayDictionaryTemplates as templates
 
 
 async def create_heartbeat(d):
@@ -9,17 +9,22 @@ async def create_heartbeat(d):
     template["d"] = d
     return pack(template)
 
+
 async def create_identify(user):
-    template_layout, template_d = templates.basic_layout, templates.identify
+    template_layout = templates.basic_layout
+    template_d = templates.identify
+
     # Putting identify structure into basic structure
     template_layout["d"] = template_d
+
     # Changing data
-    template_layout["d"]["token"] = user.bot_token
+    template_layout["d"]["token"] = user.bot_data["token"]
     template_layout["d"]["$os"] = system()
     template_layout["op"] = 2
-    template_layout["s"] = user.s
+    template_layout["s"] = user.gateway_data["s"]
     template_layout["t"] = None
     return pack(template_layout)
+
 
 def pack(dictionary):
     return json.dumps(dictionary)
