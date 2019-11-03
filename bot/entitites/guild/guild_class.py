d1f4a41a23df68c0efb38583006935cac6edbb67
CodeM01 <47262039+CodeM01@users.noreply.github.com>
2019-11-03 19:44:02 +0000

    Update guild_class.py

--- bot/entitites/guild/guild_class.py
+++ bot/entitites/guild/guild_class.py
@@ -1,13 +1,11 @@
-from bot.http_request import http_request
-
 class Guild:
 
     def __init__(self, user, guild_id):
         self.guild_id = guild_id
         self.user = user
-        self.response = None
 
     def get_channels(self):
-        request = http_request("https://discordapp.com/api/guilds/" + self.guild_id + "/channels", "GET",
-                               self.user.bot_data["token"])
-        return request
+        #request = http_request("guilds/" + self.guild_id + "/channels", "GET",
+                               #self.user.bot_data["token"])
+        #return request
+        pass
