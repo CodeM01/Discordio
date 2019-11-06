efe66b6e7f392b0e9a2b8db991c0a74627762327
CodeM01 <47262039+CodeM01@users.noreply.github.com>
2019-11-06 18:18:40 +0000

    Update channel_class.py

--- bot/entitites/channel/channel_class.py
+++ bot/entitites/channel/channel_class.py
@@ -1,3 +1,4 @@
+from http_request import http_request
 import asyncio
 
 class Channel:
@@ -6,18 +7,12 @@ class Channel:
         self.channel_id = channel_id
         self.user = user
 
-        """ Getting all of the channel data """
-
+    async def get_channel_data(self):
         load = {"request_type": "GET",
-                    "end_point": "/channels/609858340876451947",
-                    "headers": {"Authorization": "Bot " + user.bot_data["token"]}}
+                "end_point": "/channels/" + self.channel_id,
+                "headers": {"Authorization": "Bot " + self.user.bot_data["token"]}}
 
-        response = asyncio.get_event_loop().run_until_complete(user.aiohttp_client_session.http_request(load))
-        #http_request("channels/" + self.channel_id, "GET", user.bot_data["token"])
-
-        #self.name = response["name"]
-        #self.topic = response["topic"]
-        print(response)
+        return await self.user.aiohttp_client_session.http_request(load)
 
     async def send(self):
         response = http_request("channels/" + self.channel_id + "/messages", "POST", self.user.bot_data["token"])
