25cd2e837c70a51c05891b734ae20001fa844b78
CodeM01 <47262039+CodeM01@users.noreply.github.com>
2019-11-06 18:42:44 +0000

    Update guild_class.py

--- bot/guild_class.py
+++ bot/guild_class.py
@@ -4,5 +4,9 @@ class Guild:
         self.guild_id = guild_id
         self.user = user
 
-    def get_channels(self):
-        pass
+    async def get_channels(self):
+        load = {"request_type": "GET",
+                "end_point": "/guilds/" + self.guild_id + "/channels",
+                "headers": {"Authorization": "Bot " + self.user.bot_data["token"]}}
+
+        return await self.user.aiohttp_client_session.http_request(load)
