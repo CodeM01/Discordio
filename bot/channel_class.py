e477905de7005f7bec6a6388b90c9d55b15c9e66
CodeM01 <47262039+CodeM01@users.noreply.github.com>
2019-11-10 20:23:59 +0000

    Update channel_class.py

--- bot/channel_class.py
+++ bot/channel_class.py
@@ -1,12 +1,29 @@
+from json_management import gateway_dictionary_templates
+
 class Channel:
 
     def __init__(self, user, channel_id):
         self.channel_id = channel_id
         self.user = user
 
-    async def get_channel_data(self):
-        load = {"request_type": "GET",
-                "end_point": "/channels/" + self.channel_id,
-                "headers": {"Authorization": "Bot " + self.user.bot_data["token"]}}
+    async def send(self, message):
+        http_template = gateway_dictionary_templates.http_load
+        message_template = gateway_dictionary_templates.new_message
 
-        return await self.user.aiohttp_client_session.http_request(load)
+        message_template["content"] = message
+        message_template["tts"] = False
+
+        http_template["request_type"] = "POST"
+        http_template["end_point"] = "/channels/" + self.channel_id + "/messages"
+        http_template["headers/data"] = message_template
+
+        return await self.user.aiohttp_client_session.http_request(http_template)
+
+    async def get_channel_data(self):
+        http_template = gateway_dictionary_templates.http_load
+
+        http_template["request_type"] = "GET"
+        http_template["end_point"] = "/channels/" + self.channel_id
+        http_template["headers/data"] = {"Authorization": "Bot " + self.user.bot_data["token"]}
+
+        return await self.user.aiohttp_client_session.http_request(http_template)
