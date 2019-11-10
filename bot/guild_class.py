f77bacd751993151151430190781b5c535048c57
CodeM01 <47262039+CodeM01@users.noreply.github.com>
2019-11-10 20:24:29 +0000

    Update guild_class.py

--- bot/guild_class.py
+++ bot/guild_class.py
@@ -1,3 +1,5 @@
+from json_management import gateway_dictionary_templates
+
 class Guild:
 
     def __init__(self, user, guild_id):
@@ -5,8 +7,11 @@ class Guild:
         self.user = user
 
     async def get_channels(self):
-        load = {"request_type": "GET",
-                "end_point": "/guilds/" + self.guild_id + "/channels",
-                "headers": {"Authorization": "Bot " + self.user.bot_data["token"]}}
+        http_template = gateway_dictionary_templates.http_load
 
-        return await self.user.aiohttp_client_session.http_request(load)
+        http_template["request_type"] = "GET"
+        http_template["end_point"] = "/guilds/" + self.guild_id + "/channels"
+        http_template["headers/data"] = {"Authorization": "Bot " + self.user.bot_data["token"]}
+
+        response = await self.user.aiohttp_client_session.http_request(http_template)
+        return response
