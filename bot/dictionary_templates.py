f1456fdf973a26c17769a3c77642caedabc3542b
CodeM01 <47262039+CodeM01@users.noreply.github.com>
2020-04-25 14:46:39 +0100

    Create dictionary_templates.py

---
+++ bot/dictionary_templates.py
@@ -0,0 +1,36 @@
+basic_layout = {
+        "op": "",
+        "d": None,
+        "s": "",
+        "t": ""
+}
+
+heart_beat = {
+    "op": 1,
+    "d": None
+}
+
+identify = {
+    "token": "my_token",
+    "properties": {
+        "$os": "linux",
+        "$browser": "Discordio",
+        "$device": "Discordio"
+    }
+}
+
+http_load = {
+   "request_type": "",
+   "end_point": "",
+   "headers/data": ""
+}
+
+
+new_message = {
+  "content": "Hello, World!",
+  "tts": False,
+  "embed": {
+    "title": "",
+    "description": ""
+  }
+}
